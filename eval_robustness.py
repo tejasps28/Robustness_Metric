457d094239eedb51fcc4c92b25b398f8dd9fc7fe
adrienzhh <adrienzhh@gmail.com>
2024-09-29 23:08:18 -0400

    clean and update

--- eval_robustness.py
+++ eval_robustness.py
@@ -5,7 +5,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(script_dir, 'script'))
 sys.path.append(os.path.join(script_dir, 'python_wrapper'))
 
-from evo.core import metrics
+from evo.core import metrics, trajectory
 from evo.core.units import Unit
 from evo.tools import log, plot, file_interface
 from evo.tools.settings import SETTINGS
@@ -16,11 +16,15 @@ import matplotlib.pyplot as plt
 import torch
 import RobustMetricLib 
 from scipy.spatial.transform import Rotation
+from evo.core.metrics import PoseRelation
+import evo.main_rpe as main_rpe
 
-# log.configure_logging(verbose=True, debug=True, silent=False)
+import numpy as np
+
 
 print(f"Current working directory: {os.getcwd()}")
 config = ConfigFactory.parse_file('config.conf')
+
 
 max_diff = config.get_float('parameters.max_diff')
 trans_threshold = config.get_float('parameters.trans_threshold')
@@ -28,8 +32,8 @@ rot_threshold = config.get_float('parameters.rot_threshold')
 threshold_start = config.get_float('parameters.threshold_start')
 threshold_end = config.get_float('parameters.threshold_end')
 threshold_interval = config.get_float('parameters.threshold_interval')
-interv_ns = config.get_int('parameters.interv_ns', 1e7)  # default to 10ms 
-keep_freq = config.get_int('parameters.keep_freq', 10)  # default to 10 
+interv_ns = config.get_int('parameters.interv_ns', 1e7)  
+keep_freq = config.get_int('parameters.keep_freq', 10)  
 
 def format_results(ref_file, est_file, fscore_trans, fscore_rot, auc_result):
     result = "-------------\n"
@@ -38,56 +42,88 @@ def format_results(ref_file, est_file, fscore_trans, fscore_rot, auc_result):
     for t, ft, fr in zip(auc_result['thresholds'], 
                          auc_result['fscore_transes'], 
                          auc_result['fscore_rots']):
-        result += f"{t:9.2f} | {ft:16.4f} | {fr:12.4f}\n"
+        result += f"{t:9.3f} | {ft:16.4f} | {fr:12.4f}\n"
     result += "-----------+------------------+--------------\n"
-    result += f"AUC (Trans): {auc_result['fscore_area_trans'].item():.4f}\n"
-    result += f"AUC (Rot)  : {auc_result['fscore_area_rot'].item():.4f}\n"
+
+    fscore_area_trans = auc_result['fscore_area_trans'].item() if hasattr(auc_result['fscore_area_trans'], 'item') else auc_result['fscore_area_trans']
+    fscore_area_rot = auc_result['fscore_area_rot'].item() if hasattr(auc_result['fscore_area_rot'], 'item') else auc_result['fscore_area_rot']
+
+    result += f"AUC (Trans): {fscore_area_trans:.4f}\n"
+    result += f"AUC (Rot)  : {fscore_area_rot:.4f}\n"
     return result
 
 def process_trajectory_pair(ref_file, est_file):
     traj_ref = file_interface.read_tum_trajectory_file(ref_file)
+    full_len = len(traj_ref.positions_xyz)
+    print("Ref Size (Complete length): ", full_len)
+
     traj_est = file_interface.read_tum_trajectory_file(est_file)
-    traj_ref, traj_est = sync.associate_trajectories(traj_ref, traj_est, max_diff)
-    # print(traj_est.orientations_quat_wxyz)
-    # print(traj_ref)
-    def create_pose_tuple(quat, trans):
-        return (tuple(quat), tuple(trans))
+    if traj_est.timestamps[0] > traj_ref.timestamps[0]:
+        offset_2 = -(traj_est.timestamps[0] - traj_ref.timestamps[0])
+    else:
+        offset_2 = (traj_ref.timestamps[0] - traj_est.timestamps[0])
 
-    poses_ref = [create_pose_tuple(quat, trans) 
-                 for quat, trans in zip(traj_ref.orientations_quat_wxyz, traj_ref.positions_xyz)]
-    poses_est = [create_pose_tuple(quat, trans) 
-                 for quat, trans in zip(traj_est.orientations_quat_wxyz, traj_est.positions_xyz)]
+    traj_ref, traj_est = sync.associate_trajectories(traj_ref, traj_est, max_diff, offset_2)
+    print("Est Size: ", len(traj_est.positions_xyz))
 
-    trans_deriv_ref, rot_deriv_ref = RobustMetricLib.estimate_velo(interv_ns, poses_ref, keep_freq)
-    trans_deriv_est, rot_deriv_est = RobustMetricLib.estimate_velo(interv_ns, poses_est, keep_freq)
+    result_trans = main_rpe.rpe(traj_ref, traj_est, est_name='RPE translation',
+                            pose_relation=PoseRelation.translation_part, 
+                            delta=1.0, delta_unit=Unit.frames,
+                            all_pairs=False, align=True, correct_scale=False,
+                            support_loop=False)
+    
+
+    rpe_trans = metrics.RPE(PoseRelation.translation_part, delta=1.0, delta_unit=Unit.frames, all_pairs=False)
+    rpe_trans.process_data((traj_ref, traj_est))
+    
+    rpe_rot = metrics.RPE(PoseRelation.rotation_angle_deg, delta=1.0, delta_unit=Unit.frames, all_pairs=False)
+    rpe_rot.process_data((traj_ref, traj_est))
+    rpe_trans_result = result_trans.np_arrays["error_array"]
+    rpe_rot_result = np.radians(rpe_rot.error)
+
+
+    fscore_trans, fscore_rot = RobustnessMetric.calc_fscore(
+        rpe_trans_result, rpe_rot_result, full_len, trans_threshold, rot_threshold
+    )
+
+    auc_result = RobustnessMetric.eval_robustness_batch(
+        rpe_trans_result, rpe_rot_result, full_len, threshold_start, threshold_end, threshold_interval)
+
+    # def create_pose_tuple(quat, trans):
+    #     return (tuple(quat), tuple(trans))
+
+    # poses_ref = [create_pose_tuple(quat, trans) 
+    #              for quat, trans in zip(traj_ref.orientations_quat_wxyz, traj_ref.positions_xyz)]
+    # poses_est = [create_pose_tuple(quat, trans) 
+    #              for quat, trans in zip(traj_est.orientations_quat_wxyz, traj_est.positions_xyz)]
+
+    # trans_deriv_ref, rot_deriv_ref = RobustMetricLib.estimate_velo(interv_ns, poses_ref, keep_freq)
+    # trans_deriv_est, rot_deriv_est = RobustMetricLib.estimate_velo(interv_ns, poses_est, keep_freq)
 
     # trans_deriv1 = [torch.tensor(pos) for pos in traj_est.positions_xyz]
     # trans_deriv2 = [torch.tensor(pos) for pos in traj_ref.positions_xyz]
     # rot_deriv1 = [torch.tensor([0.0, 0.0, 0.0]) for _ in traj_est.positions_xyz]
     # rot_deriv2 = [torch.tensor([0.0, 0.0, 0.0]) for _ in traj_ref.positions_xyz]
 
-    trans_deriv1 = [torch.tensor(v) for v in trans_deriv_est]
-    trans_deriv2 = [torch.tensor(v) for v in trans_deriv_ref]
-    rot_deriv1 = [torch.tensor(v) for v in rot_deriv_est]
-    rot_deriv2 = [torch.tensor(v) for v in rot_deriv_ref]
+    # trans_deriv1 = [torch.tensor(v) for v in trans_deriv_est]
+    # trans_deriv2 = [torch.tensor(v) for v in trans_deriv_ref]
+    # rot_deriv1 = [torch.tensor(v) for v in rot_deriv_est]
+    # rot_deriv2 = [torch.tensor(v) for v in rot_deriv_ref]
 
-    fscore_trans, fscore_rot = RobustnessMetric.calc_fscore(
-        trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2, trans_threshold, rot_threshold
-    )
+    # print("trans_deriv1[5] ", trans_deriv1[5])
 
-    auc_result = RobustnessMetric.eval_robustness_batch(
-        trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2,
-        threshold_start, threshold_end, threshold_interval
-    )
+    # fscore_trans, fscore_rot = RobustnessMetric.calc_fscore(
+    #     trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2, trans_threshold, rot_threshold
+    # )
 
     return fscore_trans, fscore_rot, auc_result
+
 
 trajectory_pairs = config.get_list('trajectory_pairs')
 for i, pair in enumerate(trajectory_pairs):
     ref_file = pair.get_string('reference')
     est_file = pair.get_string('estimated')
-    
-    # print(f"\nProcessing trajectory pair {i+1}:")
+
     print(f"Reference: {ref_file}")
     print(f"Estimated: {est_file}")
     
@@ -95,8 +131,6 @@ for i, pair in enumerate(trajectory_pairs):
     formatted_results = format_results(ref_file, est_file, fscore_trans, fscore_rot, auc_result)
     
     print("---------------------------------")
-    # print(f"F-score for translation: {fscore_trans}")
-    # print(f"F-score for rotation: {fscore_rot}")
     print("\nAUC Result:")
     print(formatted_results)
     
--- script/robustness.py
+++ script/robustness.py
@@ -1,6 +1,15 @@
 import torch
 import os
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from pyhocon import ConfigFactory
+from evo.core import sync
+from evo.tools import file_interface, plot
+from evo.core import metrics
+from evo.core.trajectory import PosePath3D
+from evo.core.metrics import PoseRelation
+from evo.core.result import Result
 
 class RobustnessMetric:
     def calc_fscore(trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2, trans_threshold, rot_threshold):
@@ -52,23 +61,53 @@ class RobustnessMetric:
 
         return fscore_trans, fscore_rot
     
-    def eval_robustness_batch(trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2, 
-                              threshold_start, threshold_end, threshold_interval):
+    def calc_fscore(rpe_trans, rpe_rots, full_len, trans_threshold, rot_threshold):
         """
-        Evaluate robustness by calculating F-scores and AUC for a range of thresholds.
+        Calculate the F-score for velocity and angular velocity .
 
         Parameters:
-        trans_deriv1 (list of torch.Tensor): Estimated velocity.
-        rot_deriv1 (list of torch.Tensor): Estimated angular velocity.
-        trans_deriv2 (list of torch.Tensor): Ground truth velocity.
-        rot_deriv2 (list of torch.Tensor): Ground truth angular velocity.
-        threshold_start (float): Starting threshold value.
-        threshold_end (float): Ending threshold value.
-        threshold_interval (float): Interval between thresholds.
-
+        trans_deriv1 (list of torch.Tensor): estimate velocity.
+        rot_deriv1 (list of torch.Tensor): estimated angular velocity.
+        trans_deriv2 (list of torch.Tensor): GT velocity.
+        rot_deriv2 (list of torch.Tensor): GT angular velocity.
+        trans_threshold (float)
+        rot_threshold (float)
         Returns:
-        dict: Dictionary containing the F-scores and areas under the F-score curve for translational and rotational derivatives.
+        tuple: F-score for translational derivatives and F-score for rotational derivatives.
         """
+        trans_num = 0
+        rot_num = 0
+        index = 0
+
+        while index < len(rpe_trans) and index < full_len:
+            trans_val = rpe_trans[index]
+            rot_val = rpe_rots[index]
+
+            if trans_val <= trans_threshold:
+                trans_num += 1
+            if rot_val <= rot_threshold:
+                rot_num += 1
+
+            index += 1
+
+        precision_trans = trans_num / len(rpe_trans)
+        precision_rot = rot_num / len(rpe_rots)
+
+        recall_trans = trans_num / full_len
+        recall_rot = rot_num / full_len
+
+        fscore_trans = 0.0
+        if precision_trans + recall_trans > 0:
+            fscore_trans = (2 * precision_trans * recall_trans) / (precision_trans + recall_trans)
+
+        fscore_rot = 0.0
+        if precision_rot + recall_rot > 0:
+            fscore_rot = (2 * precision_rot * recall_rot) / (precision_rot + recall_rot)
+
+        return fscore_trans, fscore_rot
+    
+    def eval_robustness_batch(rpe_trans, rpe_rots, full_len, threshold_start, threshold_end, threshold_interval):
+     
         fscore_area_trans = 0.0
         fscore_area_rot = 0.0
         fscore_transes = []
@@ -78,9 +117,12 @@ class RobustnessMetric:
         
         threshold = threshold_start
         while threshold <= threshold_end:
+            threshold_value = torch.exp(torch.tensor(-10.0 * threshold))
             fscore_trans, fscore_rot = RobustnessMetric.calc_fscore(
-                trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2, threshold, threshold)
+                rpe_trans, rpe_rots, full_len, threshold_value, threshold_value)
             x_axis_len = torch.exp(torch.tensor(-10.0 * (threshold - threshold_interval * 0.5))) - torch.exp(torch.tensor(-10.0 * (threshold + threshold_interval * 0.5)))
+            if (threshold - threshold_interval * 0.5) < 0.0:
+                x_axis_len = 0.0
             fscore_area_trans += fscore_trans * x_axis_len
             fscore_area_rot += fscore_rot * x_axis_len
             fscore_transes.append(fscore_trans)
@@ -88,38 +130,92 @@ class RobustnessMetric:
             thresholds.append(threshold)
             num += 1
             threshold += threshold_interval
-
         return {
             'fscore_transes': fscore_transes,
             'fscore_rots': fscore_rots,
             'thresholds': thresholds,
             'fscore_area_trans': fscore_area_trans,
             'fscore_area_rot': fscore_area_rot
-        }
+    }
+    
+    # def eval_robustness_batch(trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2, 
+    #                           threshold_start, threshold_end, threshold_interval):
+    #     """
+    #     Evaluate robustness by calculating F-scores and AUC for a range of thresholds.
+
+    #     Parameters:
+    #     trans_deriv1 (list of torch.Tensor): Estimated velocity.
+    #     rot_deriv1 (list of torch.Tensor): Estimated angular velocity.
+    #     trans_deriv2 (list of torch.Tensor): Ground truth velocity.
+    #     rot_deriv2 (list of torch.Tensor): Ground truth angular velocity.
+    #     threshold_start (float): Starting threshold value.
+    #     threshold_end (float): Ending threshold value.
+    #     threshold_interval (float): Interval between thresholds.
+
+    #     Returns:
+    #     dict: Dictionary containing the F-scores and areas under the F-score curve for translational and rotational derivatives.
+    #     """
+    #     fscore_area_trans = 0.0
+    #     fscore_area_rot = 0.0
+    #     fscore_transes = []
+    #     fscore_rots = []
+    #     thresholds = []
+    #     num = 0
+        
+    #     threshold = threshold_start
+    #     while threshold <= threshold_end:
+    #         scale_threshold = torch.exp(torch.tensor(-10.0 * threshold))
+    #         fscore_trans, fscore_rot = RobustnessMetric.calc_fscore(
+    #             trans_deriv1, rot_deriv1, trans_deriv2, rot_deriv2, threshold, threshold)
+    #         x_axis_len = torch.exp(torch.tensor(-10.0 * (threshold - threshold_interval * 0.5))) - torch.exp(torch.tensor(-10.0 * (threshold + threshold_interval * 0.5)))
+    #         fscore_area_trans += fscore_trans * x_axis_len
+    #         fscore_area_rot += fscore_rot * x_axis_len
+    #         fscore_transes.append(fscore_trans)
+    #         fscore_rots.append(fscore_rot)
+    #         thresholds.append(threshold)
+    #         num += 1
+    #         threshold += threshold_interval
+
+    #     return {
+    #         'fscore_transes': fscore_transes,
+    #         'fscore_rots': fscore_rots,
+    #         'thresholds': thresholds,
+    #         'fscore_area_trans': fscore_area_trans,
+    #         'fscore_area_rot': fscore_area_rot
+    #     }
 
     def save_results(ref_file, est_file, fscore_trans, fscore_rot, auc_result):
-        ref_dir = os.path.dirname(ref_file)
-        result_file = os.path.join(ref_dir, 'robustness_results.csv')
+        est_dir = os.path.dirname(est_file)
+        robustness_dir = os.path.join(est_dir, 'robustness_result')
+        
+        os.makedirs(robustness_dir, exist_ok=True)
+        est_filename = os.path.splitext(os.path.basename(est_file))[0]
+        result_file = os.path.join(robustness_dir, f'robustness_results_{est_filename}.csv')
 
         with open(result_file, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             
-            writer.writerow(['Reference', ref_file])
-            writer.writerow(['Estimated', est_file])
-            writer.writerow(['F-score for translation', f'{fscore_trans:.4f}'])
-            writer.writerow(['F-score for rotation', f'{fscore_rot:.4f}'])
-            writer.writerow([])  
+            writer.writerow(['Estimated File', est_file])
+            writer.writerow([])  # Empty row for separation
             
-            writer.writerow(['AUC Result'])
             writer.writerow(['Thresholds', 'F-score (Trans)', 'F-score (Rot)'])
             
             for t, ft, fr in zip(auc_result['thresholds'], 
-                                 auc_result['fscore_transes'], 
-                                 auc_result['fscore_rots']):
-                writer.writerow([f'{t:.2f}', f'{ft:.4f}', f'{fr:.4f}'])
+                                auc_result['fscore_transes'], 
+                                auc_result['fscore_rots']):
+                writer.writerow([f'{t:.3f}', f'{ft:.4f}', f'{fr:.4f}'])
             
-            writer.writerow([])  
+            writer.writerow([])  # Empty row for separation
             writer.writerow(['AUC (Trans)', f"{auc_result['fscore_area_trans'].item():.4f}"])
             writer.writerow(['AUC (Rot)', f"{auc_result['fscore_area_rot'].item():.4f}"])
 
         print(f"Results saved to: {result_file}")
+
+    
+    def calculate_and_plot_rpe(traj_ref, traj_est, pose_relation, delta, delta_unit, all_pairs):
+        est_name = "RPE Test"
+        result = metrics.compute_rpe(traj_ref, traj_est, pose_relation, delta, delta_unit, all_pairs)
+        
+
+        return result
+        
